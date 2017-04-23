bf3ff25439a1e501126776e8fc6877e9c93d6f0c
jeremy_zhou <feng4206yu@163.com>
2017-04-23 12:09:38 +0800

    as you suggested title changed to 18
    as you suggested description

--- main.c
+++ main.c
@@ -6,13 +6,13 @@ int main(void)
 {
   time_t t;
   struct tm* tmp;
-  char buf1[20];
+  char buf1[18];
   char buf2[24];
   
   time(&t);
   tmp = localtime(&t);
-  if(strftime(buf1,20,"time and date: %r, %a %b %d, %Y", tmp) == 0)
-    printf("buffer length 20 is too small\n");
+  if(strftime(buf1,18,"time and date: %r, %a %b %d, %Y", tmp) == 0)
+    printf("buffer length 18 is too small\n");
     
   exit(0);
 }
