82c60bc7d0afd847c13fb142b323e0701410b5f0
jeremy_zhou <feng4206yu@163.com>
2017-04-23 11:40:01 +0800

    change predefined value
    20 would be better

--- main.c
+++ main.c
@@ -6,13 +6,13 @@ int main(void)
 {
   time_t t;
   struct tm* tmp;
-  char buf1[16];
+  char buf1[20];
   char buf2[24];
   
   time(&t);
   tmp = localtime(&t);
-  if(strftime(buf1,16,"time and date: %r, %a %b %d, %Y", tmp) == 0)
-    printf("buffer length 16 is too small\n");
+  if(strftime(buf1,20,"time and date: %r, %a %b %d, %Y", tmp) == 0)
+    printf("buffer length 20 is too small\n");
     
   exit(0);
 }
